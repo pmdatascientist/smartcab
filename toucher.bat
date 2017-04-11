ec34782055ec137a237c8c42ae11cc39e2cf7dd3
pmdatascientist <puneet.mathurs@gmail.com>
2017-04-11 23:03:37 +0530

    chore: updating build tasks, package manager configs, ; no production code change Change made on: 2017_04_11__23_03_35.txt

--- toucher.bat
+++ toucher.bat
@@ -1,12 +1,14 @@
+set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
+echo %datetimef%
 FOR /F "TOKENS=1 eol=/ DELIMS=/ " %%A IN ('DATE/T') DO SET dd=%%A
 FOR /F "TOKENS=1,2 eol=/ DELIMS=/ " %%A IN ('DATE/T') DO SET mm=%%B
 FOR /F "TOKENS=1,2,3 eol=/ DELIMS=/ " %%A IN ('DATE/T') DO SET yyyy=%%C
-SET todaysdate=%yyyy%%mm%%dd%
+SET todaysdate=%yyyy%%mm%%dd%%TIME%
 echo %dd%
 echo %mm%
 echo %yyyy%
 echo %todaysdate%
-SET todaysdate=%todaysdate% & ".txt"
+SET todaysdate=%datetimef%".txt"
 type nul > %todaysdate%
 date /t >> %todaysdate%
 date /t >> %todaysdate%
